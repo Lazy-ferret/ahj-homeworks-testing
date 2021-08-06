668007efafdffe9d90955084d0ac93c879f823f4
Lazy-ferret <a.veselov83@gmail.com>
2021-08-06 23:29:15 +0300

    Initial commit

---
+++ src/js/__tests__/app.test.js
@@ -0,0 +1,34 @@
+import checkLuhn from '../components/CheckLuhn.js';
+import checkPayment from '../components/CheckPayment.js';
+
+test('checkLuhn should return true', () => {
+  expect(checkLuhn('4893 4702 1277 8911')).toBe(true);
+});
+
+test('checkLuhn should return false', () => {
+  expect(checkLuhn('4893 4702 1277 8912')).toBe(false);
+});
+
+test.each([
+  ['2200240728327020', true],
+  ['2200240728327021', false],
+  ['2200240728327022', false],
+])('should return correct result for %s', (card, result) => {
+  expect(checkLuhn(card)).toBe(result);
+});
+
+test('should return visa', () => {
+  expect(checkPayment(4893470212778911)).toBe('visa');
+});
+
+test.each([
+  [2204222222222222, 'mir'],
+  [4444444444444, 'visa'],
+  [5201111111111111, 'mastercard'],
+  [2301111111111111, 'mastercard'],
+  [347000000000000, 'amex'],
+  [3011111111111119, 'diners'],
+  [3500000000000000, 'jcb'],
+])('should return correct result for %s', (card, result) => {
+  expect(checkPayment(card)).toBe(result);
+});
